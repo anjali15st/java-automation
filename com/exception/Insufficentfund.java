d87bbd0eefd533add3de4ddf3f2c8aeee39f5833
anjali15st <anjalisheth143@gmail.com>
2023-03-29 16:15:28 +0530

    merge

--- com/exception/Insufficentfund.java
+++ com/exception/Insufficentfund.java
@@ -1,5 +1,14 @@
 package com.exception;
 
-public class Insufficentfund {
-
+public class Insufficentfund extends Exception
+{
+double amount;
+public Insufficentfund(double amount) 
+{
+	this.amount=amount;
+}
+public double getamount()
+{
+	return this.amount;
+}
 }
